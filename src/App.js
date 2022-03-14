67bfa81155417d1ab7dd77af68325efcfd02d9f9
sammy-jay <sammyjayden27@gmail.com>
2022-03-14 23:29:59 +0100

    11 files modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import SingleCocktail from './pages/SingleCocktail';
+import Error from './pages/Error';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/about" element={<About />} />
+      <Route exact path="/cocktail:id" element={<SingleCocktail />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
   );
 }
